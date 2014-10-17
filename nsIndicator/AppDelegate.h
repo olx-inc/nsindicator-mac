212bfa7633f6b3d60e9471c10bff3c9b26e1f308
juansanzone <juan.sanzone@gmail.com>
2014-10-17 09:34:56 -0300

    Initial Commit

---
+++ nsIndicator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nsIndicator
+//
+//  Created by Juan on 10/17/14.
+//  Copyright (c) 2014 jsanzo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ nsIndicator/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  nsIndicator
+//
+//  Created by Juan on 10/17/14.
+//  Copyright (c) 2014 jsanzo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
